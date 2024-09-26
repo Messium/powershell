22422ac622565c4409f71ec8eb4e4dc198be0d90
messium <danielholstrom@gmail.com>
2024-09-26 08:50:51 +0200

    nicer theme

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,15 @@
 (@(& '/usr/bin/oh-my-posh' init pwsh --config='' --print) -join "`n") | Invoke-Expression
+
+# theme:
+oh-my-posh init pwsh --config '/home/messium/.config/powershell/themes/catppuccin_mocha.omp.json' | Invoke-Expression
+
+# modules:
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 Import-Module posh-git
 Import-Module -Name Terminal-Icons
 Import-Module PSReadLine
 
+# keybinds:
 # Set-Alias "ls -a" "Get-ChildItem . -Force"
 
 Set-PSReadLineOption -PredictionSource History
@@ -11,6 +17,7 @@ Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineOption -EditMode vi
 
 Set-Alias -Name history -Value Get-PSReadlineHistory
+
 # Set-PSReadLine -MaximumHistoryCount "4096" TODO: not working bad syntax or arg none exsistant
 # Set-PSReadLine -HistorySavePath "$HOME/.config/powershell/ConsoleHost_history.txt"
 
